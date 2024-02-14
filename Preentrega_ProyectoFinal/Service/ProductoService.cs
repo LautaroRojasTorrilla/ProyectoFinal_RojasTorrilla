11a5b570807a001b0910414d59237f6edd9090c2
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2024-02-14 10:41:21 -0300

    Creacion Service

---
+++ Preentrega_ProyectoFinal/Service/ProductoService.cs
@@ -0,0 +1,113 @@
+﻿using Preentrega_ProyectoFinal.Database;
+using Preentrega_ProyectoFinal.SistemaGestionData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Preentrega_ProyectoFinal.Service
+{
+    public static class ProductoService
+    {
+        public static List<Producto> ObtenerTodosLosProductos()
+        {
+            try
+            {
+                using (CoderContext contexto = new CoderContext())
+                {
+                    List<Producto> productos = contexto.Productos.ToList();
+                    return productos;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Lanzar una nueva excepción para manejar el error en un nivel superior.
+                throw new Exception($"Error al obtener todos los productos: {ex.Message}", ex);
+            }
+        }
+
+        public static Producto ObtenerProductoPorID(int id)
+        {
+            try
+            {
+                using (CoderContext contexto = new CoderContext())
+                {
+                    Producto productoBuscado = contexto.Productos.FirstOrDefault(p => p.Id == id)
+                        ?? throw new Exception($"No se encontró un producto con ID {id}");
+
+                    return productoBuscado;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al obtener el producto por ID: {ex.Message}", ex);
+            }
+        }
+
+        public static bool AgregarProducto(Producto producto)
+        {
+            try
+            {
+                using (CoderContext contexto = new CoderContext())
+                {
+                    contexto.Productos.Add(producto);
+                    contexto.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al agregar el producto: {ex.Message}", ex);
+            }
+        }
+
+        public static bool ModificarProductoPorId(Producto producto, int id)
+        {
+            try
+            {
+                using (CoderContext contexto = new CoderContext())
+                {
+                    Producto productoBuscado = contexto.Productos.FirstOrDefault(p => p.Id == id)
+                        ?? throw new Exception($"No se encontró un producto con ID {id}");
+
+                    productoBuscado.Descripciones = producto.Descripciones;
+                    productoBuscado.Costo = producto.Costo;
+                    productoBuscado.PrecioVenta = producto.PrecioVenta;
+                    productoBuscado.Stock = producto.Stock;
+
+                    contexto.Productos.Update(productoBuscado);
+                    contexto.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al modificar el producto: {ex.Message}", ex);
+            }
+        }
+
+        public static bool EliminarProductoPorID(int Id)
+        {
+            try
+            {
+                using (CoderContext contexto = new CoderContext())
+                {
+                    Producto productoAEliminar = contexto.Productos
+                        .Include(p => p.ProductoVendidos)
+                        .FirstOrDefault(p => p.Id == Id)
+                        ?? throw new Exception($"No se encontró un producto con ID {Id}");
+
+                    contexto.Productos.Remove(productoAEliminar);
+                    contexto.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al eliminar el producto: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Preentrega_ProyectoFinal/Service/UsuarioService.cs
@@ -0,0 +1,100 @@
+﻿using Preentrega_ProyectoFinal.Database;
+using Preentrega_ProyectoFinal.SistemaGestionData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Preentrega_ProyectoFinal.Service
+{
+    public static class UsuarioService
+    {
+        public static List<Usuario> ObtenerTodosLosUsuarios()
+        {
+            using (CoderContext contexto = new CoderContext())
+            {
+                List<Usuario> usuarios = contexto.Usuarios.ToList();
+
+                return usuarios;
+            }
+        }
+
+        public static Usuario ObtenerUsuarioporID(int id)
+        {
+            using (CoderContext contexto = new CoderContext())
+            {
+                //añ where le paso una fn criterio
+                Usuario? usuarioBuscado = contexto.Usuarios.Where(u => u.Id == id).FirstOrDefault();
+
+                return usuarioBuscado;
+            }
+        }
+
+        public static Usuario ObtenerUsuarioporID2(int id)
+        {
+            List<Usuario> usuarios = UsuarioService.ObtenerTodosLosUsuarios();
+
+            foreach (Usuario item in usuarios)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool AgregarUsuario(Usuario usuario)
+        {
+            using (CoderContext contexto = new CoderContext())
+            {
+                contexto.Usuarios.Add(usuario);
+
+                //si no hago esta linea no tiene impacto en la base de datos.
+                contexto.SaveChanges();
+                return true;
+            }
+        }
+
+        public static bool ModificarUsuarioPorId(Usuario usuario, int id)
+        {
+
+            using (CoderContext contexto = new CoderContext())
+            {
+                Usuario? usuarioBuscado = contexto.Usuarios.Where(u => u.Id == id).FirstOrDefault();
+
+                usuarioBuscado.Nombre = usuario.Nombre;
+                usuarioBuscado.NombreUsuario = usuario.NombreUsuario;
+                usuarioBuscado.Apellido = usuario.Apellido;
+                usuarioBuscado.Mail = usuario.Mail;
+
+                contexto.Usuarios.Update(usuarioBuscado);
+
+                contexto.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public static bool EliminarUsuarioPorID(int id)
+        {
+            using (CoderContext contexto = new CoderContext())
+            {
+                Usuario? usuarioAEliminar = contexto.Usuarios.FirstOrDefault(u => u.Id == id);
+
+                if (usuarioAEliminar is not null)
+                {
+                    contexto.Usuarios.Remove(usuarioAEliminar);
+                    contexto.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
